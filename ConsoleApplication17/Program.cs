4fa080b6d3610d966c5ae7f3c37fe1cc90549015
Сергей Шабельник <honning69@MacBook-Pro-Sergej.local>
2019-03-03 17:26:01 +0300

    Initial commit

---
+++ ConsoleApplication17/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication17
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            ZhegalkinPolinom zh = new ZhegalkinPolinom("x1x2x3+1+x2x3");
+            zh.Insert(new Konj(12));
+            zh.SortByLength();
+            Console.WriteLine(zh.Polinom[0].Info);
+            Console.WriteLine(zh.Polinom[1].Info);
+            Console.WriteLine(zh.Polinom[2].Info);
+            Console.WriteLine(zh.Polinom[3].Info);
+            Console.WriteLine(zh.ToString());
+        }
+    }
+    
+    public class ZhegalkinPolinom
+    {
+        public List<Konj> Polinom;
+
+        public ZhegalkinPolinom(string s)
+        {
+            Polinom = new List<Konj>();
+            var a = s.Split('+');
+            foreach (var i in a)
+            {
+                string result = null;
+                if (i == "1")
+                {
+                    result += 0;
+                    Polinom.Add(new Konj(int.Parse(result)));
+                    continue;
+                }
+                foreach (var j in i)
+                {
+                    if (Char.IsDigit(j))
+                        result += j;    
+                }
+                if (result != null)
+                    Polinom.Add(new Konj(int.Parse(result)));
+            }
+
+        }
+        
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var i in Polinom)
+            {
+                sb.Append(i.Info + " ");
+            }
+
+            var temporary = sb.ToString();
+            var temp = temporary.Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+            string result = null;
+            int counter = 1;
+            foreach (var j in temp)
+            {
+                foreach (var z in j)
+                {
+                    if (z == '0')
+                        result += 1;
+                    else
+                        result += "x" + z;
+                }
+                if (temp.Length > counter)
+                {
+                    result += "+";
+                    counter++;
+                }
+            }
+            if (result != null)
+                return result;
+            throw new Exception("Полином Жегалкина пуст");
+        }
+
+        public void Insert(Konj k)
+        {
+            foreach (var m in Polinom)
+            {
+                if (m.Info == k.Info)
+                    throw new Exception("Полином уже содержит данный конъюнкт");
+            }
+            Polinom.Add(k);
+        }
+
+        public void SortByLength()
+        {
+            Polinom = (from j in Polinom orderby j.Info descending select j).ToList();
+        }
+    }
+
+    public class Konj : List<int>
+    {
+        public int Info;
+        public Konj(int info)
+        {
+            Info = info;
+        }
+        
+    }
+}
